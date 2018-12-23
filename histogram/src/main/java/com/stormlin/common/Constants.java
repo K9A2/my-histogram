330ba7c37ca7b7e5668bad6d0ac66e8f980c9670
stormlin <lin-jinting@qq.com>
2018-12-23 15:59:05 +0800

    Fixed some errors in histogram types

--- histogram/src/main/java/com/stormlin/common/Constants.java
+++ histogram/src/main/java/com/stormlin/common/Constants.java
@@ -2,9 +2,9 @@ package main.java.com.stormlin.common;
 
 public class Constants {
 
-    public static final String SIMPLE_BAR_CHART = "simpleBarChart";
-    public static final String GROUPED_BAR_CHART = "groupedBarChart";
-    public static final String STACKED_BAR_CHART = "stackedBarChart";
+    public static final String SIMPLE_BAR_CHART = "SimpleBarChart";
+    public static final String GROUPED_BAR_CHART = "GroupedBarChart";
+    public static final String STACKED_BAR_CHART = "StackedBarChart";
 
     public static final String DEFAULT_FONT_NAME = "Consolas";
     public static final int DEFAULT_FONT_SIZE = 16;
