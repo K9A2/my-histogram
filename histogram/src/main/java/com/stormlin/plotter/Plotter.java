7152b814b01d17e36e2ece95e4dc11dd94de8c1f
stormlin <lin-jinting@qq.com>
2018-12-24 17:12:57 +0800

    Added center-vertical aligned labels for all YAxes

--- histogram/src/main/java/com/stormlin/plotter/Plotter.java
+++ histogram/src/main/java/com/stormlin/plotter/Plotter.java
@@ -1,5 +1,6 @@
 package main.java.com.stormlin.plotter;
 
+import javafx.scene.transform.Affine;
 import main.java.com.stormlin.common.Constants;
 import main.java.com.stormlin.histogram.Histogram;
 import main.java.com.stormlin.histogram.HistogramYAxis;
@@ -7,6 +8,7 @@ import main.java.com.stormlin.histogram.HistogramTitle;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
 class Plotter extends JPanel {
@@ -38,20 +40,31 @@ class Plotter extends JPanel {
         int coordinateY = (int) (histogram.getCanvasHeight() * (1 - histogram.getMargins()[Constants.MARGIN_BOTTOM]));
         int max = (int) leftAxis.getMax();
         int min = (int) leftAxis.getMin();
-        int lineLength = Constants.DEFAULT_MARK_LINE_LENGTH;
+        int markLineLength = Constants.DEFAULT_MARK_LINE_LENGTH;
+        int maxLineLength = 0;
         // Right-align the left ruler
         int rightEdge = (int) (coordinateX - 0.01 * histogram.getCanvasWidth());
 
         for (int i = min; i <= max; i += (int) leftAxis.getStep()) {
+            int lineLength = metrics.stringWidth(String.valueOf(i));
             // Draw the numbers, default align right
-            g.drawString(String.valueOf(i), rightEdge - metrics.stringWidth(String.valueOf(i)),
-                    (int) (coordinateY + 0.25 * metrics.getHeight()));
+            g.drawString(String.valueOf(i), rightEdge - lineLength, (int) (coordinateY + 0.25 * metrics.getHeight()));
             // Draw the line mark for this ruler
-            g.drawLine(coordinateX - lineLength, coordinateY, coordinateX, coordinateY);
+            g.drawLine(coordinateX - markLineLength, coordinateY, coordinateX, coordinateY);
             coordinateY -= (int) (leftAxis.getStep() * leftAxis.getPointsPerUnit());
+            if (lineLength > maxLineLength) {
+                maxLineLength = lineLength;
+            }
         }
+        // Draw the label for left YAxis
+        int labelX = (int) (rightEdge - maxLineLength - 0.01 * histogram.getCanvasWidth());
+        int labelY = (int) (histogram.getCanvasHeight() * histogram.getMargins()[Constants.MARGIN_UPPER] +
+                histogram.getPlotAreaHeight() * 0.5);
+        AffineTransform rotateNegative90 = AffineTransform.getRotateInstance(Math.toRadians(-90), labelX, labelY);
+        plotYAxisLabel(g, rotateNegative90, leftAxis.getLabel(),
+                (int) (labelX - 0.5 * metrics.stringWidth(leftAxis.getLabel())), labelY);
 
-        HistogramYAxis rightAxis = null;
+        HistogramYAxis rightAxis;
         if (histogram.getyYAxisList().size() > 1) {
             rightAxis = histogram.getyYAxisList().get(1);
             /* Plot the right ruler */
@@ -60,11 +73,23 @@ class Plotter extends JPanel {
             max = (int) rightAxis.getMax();
             min = (int) rightAxis.getMin();
             int leftEdge = (int) (coordinateX + 0.01 * histogram.getCanvasWidth());
+            maxLineLength = 0;
             for (int i = min; i <= max; i += (int) rightAxis.getStep()) {
+                int lineLength = metrics.stringWidth(String.valueOf(i));
                 g.drawString(String.valueOf(i), leftEdge, (int) (coordinateY + 0.25 * metrics.getHeight()));
-                g.drawLine(coordinateX, coordinateY, coordinateX + lineLength, coordinateY);
+                g.drawLine(coordinateX, coordinateY, coordinateX + markLineLength, coordinateY);
                 coordinateY -= (int) (rightAxis.getStep() * rightAxis.getPointsPerUnit());
+                if (lineLength > markLineLength) {
+                    maxLineLength = lineLength;
+                }
             }
+            // Draw the label for left YAxis
+            labelX = (int) (leftEdge + maxLineLength + 0.01 * histogram.getCanvasWidth());
+            labelY = (int) (histogram.getCanvasHeight() * histogram.getMargins()[Constants.MARGIN_UPPER] +
+                    histogram.getPlotAreaHeight() * 0.5);
+            AffineTransform rotatePositive90 = AffineTransform.getRotateInstance(Math.toRadians(90), labelX, labelY);
+            plotYAxisLabel(g, rotatePositive90, rightAxis.getLabel(),
+                    (int) (labelX - 0.5 * metrics.stringWidth(leftAxis.getLabel())), labelY);
         }
     }
 
@@ -80,6 +105,12 @@ class Plotter extends JPanel {
         int titleLength = metrics.stringWidth(title.getText());
 
         g.drawString(title.getText(), (int) (coordinateX - 0.5 * titleLength), coordinateY);
+    }
+
+    void plotYAxisLabel(Graphics g, AffineTransform transform, String label, int labelX, int labelY) {
+        Graphics2D temp = (Graphics2D) g.create();
+        temp.transform(transform);
+        temp.drawString(label, labelX, labelY);
     }
 
 }
